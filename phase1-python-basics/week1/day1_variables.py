311700b670301bee06d9e2d03804f23da233aba1
Andres Espitia <andresespiti78@yahoo.com>
2026-03-16 16:54:41 -0700

    Phase 1 Week 1 Day 1 - variables and data types

---
+++ phase1-python-basics/week1/day1_variables.py
@@ -0,0 +1,29 @@
+#Day 1 - Variables and Data Types
+#QA Automation Course - Phase 1
+
+
+#--- String (Test) ---
+test_url = "https://www.example.com"
+tester_name = "Yout Name"
+
+# --- Integer (Whole Number) ---
+total_test_cases = 50
+tests_passed = 45
+tests_failed = 5
+
+# --- Float (decimal number) ---
+pass_rate = 90.0 
+
+# --- Boolean (True or False) ---
+is_test_passing = True
+is_bug_found = False
+
+# --- Printing variables to the screen ---
+print("Tester:", tester_name)
+print("Test URL:", test_url)
+print("Total Tests:", total_test_cases)
+print("Passed:", tests_passed)
+print("Failed:", tests_failed)
+print("Pass Rate:", pass_rate, "%")
+print("All Tests Passing?", is_test_passing)
+print("Bug Found?", is_bug_found)
